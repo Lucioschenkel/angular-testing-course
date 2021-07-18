999f449ac80cde58e8af91612395e7319889384c
Lucio <lucio.schenkel@acad.pucrs.br>
2021-07-18 17:28:48 -0300

    async component testing - 2

---
+++ src/app/courses/home/async-examples.spec.ts
@@ -0,0 +1,12 @@
+describe('Async Testing Examples', () => {
+  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
+    let test = false;
+
+    setTimeout(() => {
+      test = true;
+
+      expect(test).toBeTruthy();
+      done();
+    }, 1000);
+  });
+});
